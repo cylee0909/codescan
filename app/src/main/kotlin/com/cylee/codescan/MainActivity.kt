8ab7e83391c8cd0e961f949f74361e9c7e288470
cylee <cylee0909@qq.com>
2017-02-12 19:55:20 +0800

    扫描二维码改为输入框

--- app/src/main/kotlin/com/cylee/codescan/MainActivity.kt
+++ app/src/main/kotlin/com/cylee/codescan/MainActivity.kt
@@ -1,44 +1,35 @@
 package com.cylee.codescan
 
-import android.graphics.Bitmap
-import android.graphics.Rect
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.text.TextUtils
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import cn.csnbgsh.herbarium.bind
+import com.cylee.androidlib.base.BaseActivity
 import com.cylee.codescan.data.ScanCodeItem
 import com.cylee.codescan.data.ScanCollection
 import com.cylee.codescan2.R
 import com.cylee.lib.widget.dialog.DialogUtil
-import com.google.zxing.CaptureActivity
-import com.google.zxing.Result
-import com.google.zxing.camera.CameraManager
 import java.util.*
 
-class MainActivity : CaptureActivity() , View.OnClickListener{
-    var mScanPositionBn : Button? = null;
-    var mScanOtherBn : Button? = null;
-    var mPositionTipText : TextView? = null;
+class MainActivity : BaseActivity() {
     var mPositonCode : String? = "";
     var mScanList:ListView? = null
     var mAdapter : ScanAdapter? = null;
     var mScanCollection : ScanCollection? = null;
+    var mCodeEdit : EditText? = null
+    var mGoodsEdit : EditText? = null
+    var confirm : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val root = bind<FrameLayout>(R.id.qcs_root)
-        val extraView : View = layoutInflater.inflate(R.layout.code_ext, null)
-        val rect: Rect = CameraManager.get().framingRect
-        val params : FrameLayout.LayoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT)
-        params.topMargin = rect.bottom
-        root?.addView(extraView, params)
+        setContentView(R.layout.activity_newcode)
         bind<TextView>(R.id.qcs_title).text = "扫码"
         var clear = bind<TextView>(R.id.qcs_right_text)
         clear.text = "清空"
+        mCodeEdit = bind(R.id.code)
+        mGoodsEdit = bind(R.id.goods)
+        confirm = bind(R.id.confirm)
         clear.setOnClickListener {
             v ->
             DialogUtil().showDialog(this, "确认清空", "取消", "确认", object : DialogUtil.ButtonClickListener {
@@ -51,18 +42,14 @@ class MainActivity : CaptureActivity() , View.OnClickListener{
                 }
             }, "确认全部已扫描的数据？")
         }
-        init(extraView);
+        init(bind(R.id.ext))
+        confirm?.setOnClickListener {
+            handleDecode()
+        }
     }
 
     private fun init(v:View) {
-        mScanPositionBn = v.bind(R.id.scan_position);
-        mScanOtherBn = v.bind(R.id.scan_other);
-        mPositionTipText = v.bind(R.id.position_code_tip);
         mScanList = v.bind(R.id.codes)
-        mScanPositionBn?.isSelected = true;
-        mScanPositionBn?.setOnClickListener(this)
-        mScanOtherBn?.setOnClickListener(this)
-
         mAdapter = ScanAdapter()
         mScanList!!.adapter = mAdapter
         mScanList!!.setOnItemLongClickListener { adapterView, view, i, l ->
@@ -84,25 +71,6 @@ class MainActivity : CaptureActivity() , View.OnClickListener{
         }
     }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.scan_position -> {
-                mScanPositionBn?.isSelected = true;
-                mScanOtherBn?.isSelected = false;
-                reCapture();
-            }
-            R.id.scan_other -> {
-                if (TextUtils.isEmpty(mPositonCode)) {
-                    DialogUtil.showToast("请先扫描货位码", false);
-                    return
-                }
-                reCapture();
-                mScanPositionBn?.isSelected = false;
-                mScanOtherBn?.isSelected = true;
-            }
-        }
-    }
-
     override fun onStart() {
         super.onStart()
         mScanCollection = ScanCollection.readFromPreference()
@@ -115,52 +83,36 @@ class MainActivity : CaptureActivity() , View.OnClickListener{
     override fun onStop() {
         super.onStop()
         mScanCollection?.writeToPreference()
-        mHandler.removeCallbacks(recaptureRunnable)
     }
 
-    override fun handleDecode(result: Result?, barcode: Bitmap?) {
-        playBeepSoundAndVibrate()
-        if (mScanPositionBn!!.isSelected) {
-            mPositonCode = result?.text
-            mPositionTipText!!.setText("当前货位码："+mPositonCode);
-        } else if (mScanOtherBn!!.isSelected){
-            val v = View.inflate(this, R.layout.confirm_code, null);
-            val text = v.bind<TextView>(R.id.code_info_text);
-            val codeItem = formatCodeResult(result?.text);
-            if (codeItem != null) {
-                var exist = mScanCollection?.data?.contains(codeItem) ?: false;
-                if (exist) {
-                    DialogUtil.showToast("数据重复", false)
-                    mHandler.postDelayed({
-                        reCapture()
-                    }, 1000)
-                    return;
-                }
-                text.text = "流水号："+codeItem.id
-                val numEdit = v.bind<EditText>(R.id.num_edit)
-                numEdit.setText(codeItem.count.toString())
-                numEdit.setSelection(numEdit.text.length)
-                DialogUtil().showViewDialog(this, "确认信息", "取消", "确认",object : DialogUtil.ButtonClickListener {
-                    override fun OnRightButtonClick() {
-                        mHandler.postDelayed({
-                            reCapture()
-                        }, 1000)
-                        codeItem.count = numEdit.text.toString().toFloat()
-                        mScanCollection?.data?.add(codeItem)
-                        mAdapter?.notifyDataSetChanged()
-                    }
-                    override fun OnLeftButtonClick() {
-                        mHandler.postDelayed({
-                            reCapture()
-                        }, 1000)
-                    }
-                }, v)
-            } else {
-                DialogUtil.showToast("无效的货物码", false);
-                mHandler.postDelayed({
-                    reCapture()
-                }, 1000)
+    fun handleDecode() {
+        mPositonCode = mCodeEdit?.text.toString()
+        var goodsInfo = mGoodsEdit?.text.toString()
+        val v = View.inflate(this, R.layout.confirm_code, null)
+        val text = v.bind<TextView>(R.id.code_info_text)
+        val codeItem = formatCodeResult(goodsInfo)
+        if (codeItem != null) {
+            var exist = mScanCollection?.data?.contains(codeItem) ?: false
+            if (exist) {
+                DialogUtil.showToast("数据重复", false)
+                return;
             }
+            text.text = "流水号：" + codeItem.id
+            val numEdit = v.bind<EditText>(R.id.num_edit)
+            numEdit.setText(codeItem.count.toString())
+            numEdit.setSelection(numEdit.text.length)
+            DialogUtil().showViewDialog(this, "确认信息", "取消", "确认", object : DialogUtil.ButtonClickListener {
+                override fun OnRightButtonClick() {
+                    codeItem.count = numEdit.text.toString().toFloat()
+                    mScanCollection?.data?.add(codeItem)
+                    mAdapter?.notifyDataSetChanged()
+                }
+
+                override fun OnLeftButtonClick() {
+                }
+            }, v)
+        } else {
+            DialogUtil.showToast("无效的货物码", false);
         }
     }
 
@@ -175,14 +127,6 @@ class MainActivity : CaptureActivity() , View.OnClickListener{
         }
         return null
     }
-
-    private val recaptureRunnable = Runnable() {
-        run {
-            reCapture()
-        }
-    }
-
-    private val mHandler = Handler(Looper.getMainLooper());
 
     inner class ScanAdapter:BaseAdapter() {
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
